ae82239c34e1f951a3701dce4e521b64061654da
James Harton <james@automat.nz>
2018-09-04 02:52:12 +0000

    Update Elixir version and deps.

--- lib/vivid/png/buffer.ex
+++ lib/vivid/png/buffer.ex
@@ -8,34 +8,35 @@ defimpl Vivid.PNG, for: Vivid.Buffer do
   @doc """
   Convert a Vivid buffer into a PNG file.
   """
-  @spec to_png(Buffer.t, Path.t) :: :ok | {:error, any}
+  @spec to_png(Buffer.t(), Path.t()) :: :ok | {:error, any}
   def to_png(buffer, path) do
     with {:ok, file} <- File.open(path, [:write]),
-         png         <- create_png(buffer, file),
-         :ok         <- :png.close(png),
-         :ok         <- File.close(file),
+         png <- create_png(buffer, file),
+         :ok <- :png.close(png),
+         :ok <- File.close(file),
          do: :ok
   end
 
   defp config(%Buffer{rows: height, columns: width}, file) do
     %{
-      size:    {width, height},
-      mode:    {:rgba, 8},
-      file:    file,
+      size: {width, height},
+      mode: {:rgba, 8},
+      file: file
     }
   end
 
-  defp create_png(%Buffer{columns: width}=buffer, file) do
-    png = buffer
+  defp create_png(%Buffer{columns: width} = buffer, file) do
+    png =
+      buffer
       |> config(file)
-      |> :png.create
+      |> :png.create()
 
     buffer
-      |> Stream.map(&colour_to_binary(&1))
-      |> Stream.chunk(width)
-      |> Stream.map(&Enum.join(&1))
-      |> Enum.reverse
-      |> Enum.reduce(png, &(:png.append(&2, {:row, &1})))
+    |> Stream.map(&colour_to_binary(&1))
+    |> Stream.chunk(width)
+    |> Stream.map(&Enum.join(&1))
+    |> Enum.reverse()
+    |> Enum.reduce(png, &:png.append(&2, {:row, &1}))
   end
 
   defp colour_to_binary(%RGBA{red: r, green: g, blue: b, alpha: a}) do
@@ -43,6 +44,6 @@ defimpl Vivid.PNG, for: Vivid.Buffer do
     g = round(g * 255)
     b = round(b * 255)
     a = round(a * 255)
-    <<r::integer-size(8), g::integer-size(8), b::integer-size(8), a::integer-size(8) >>
+    <<r::integer-size(8), g::integer-size(8), b::integer-size(8), a::integer-size(8)>>
   end
 end
