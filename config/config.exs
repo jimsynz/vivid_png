590723935e1ec15a1ba00fc2bdf1072a776ec1bf
James Harton <james@harton.nz>
2020-12-28 20:28:28 +1300

    chore: fix git_ops config.

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :git_ops,
   mix_project: Mix.Project.get!(),
   changelog_file: "CHANGELOG.md",
-  repository_url: "https://gitlab.com/***PROJECT_PATH***",
+  repository_url: "https://gitlab.com/jimsy/vivid_png.ex",
   manage_mix_version?: true,
   manage_readme_version: "README.md",
   version_tag_prefix: "v"
